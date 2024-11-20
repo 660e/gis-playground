84e231867f87072f14bdeeebc2ddf12b2c15cd3a
Min <solunar@vip.qq.com>
2024-11-20 21:50:38 +0800

    Update

--- src/views/mapbox/index.vue
+++ src/views/mapbox/index.vue
@@ -1,3 +1,22 @@
+<script setup lang="ts">
+import 'mapbox-gl/dist/mapbox-gl.css';
+import mapboxgl from 'mapbox-gl';
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2tkNDIyNTAiLCJhIjoiY20zaXNpdnd5MDQ4bDJycTVtNXoxanlncSJ9.7nLVOvqCIN3K1imhm9Carw';
+
+onMounted(() => {
+  const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v12',
+    center: [-74.5, 40],
+    zoom: 9,
+  });
+  console.log(map);
+});
+</script>
+
 <template>
-  <div>mapbox</div>
+  <div class="h-screen overflow-hidden">
+    <div class="h-full" id="map"></div>
+  </div>
 </template>
