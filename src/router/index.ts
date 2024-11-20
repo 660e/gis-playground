09bcaffa373c9697317df28636d3cdec31297ae7
Min <solunar@vip.qq.com>
2024-11-20 21:14:12 +0800

    Update

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,6 +8,11 @@ const router = createRouter({
       name: 'home',
       component: () => import('@/views/home.vue'),
     },
+    {
+      path: '/leaflet',
+      name: 'leaflet',
+      component: () => import('@/views/leaflet/index.vue'),
+    },
   ],
 });
 
--- src/views/leaflet/index.vue
+++ src/views/leaflet/index.vue
@@ -1,3 +1,3 @@
 <template>
-  <div></div>
+  <div>leaflet</div>
 </template>
