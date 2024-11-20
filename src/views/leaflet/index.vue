46e8ad064365c82056f27096c9adfa6d75b4478c
Min <solunar@vip.qq.com>
2024-11-20 21:38:04 +0800

    Update

--- src/views/leaflet/index.vue
+++ src/views/leaflet/index.vue
@@ -1,3 +1,15 @@
+<script setup lang="ts">
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
+onMounted(() => {
+  const map = L.map('map', { attributionControl: false }).setView([51.505, -0.09], 13);
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+});
+</script>
+
 <template>
-  <div>leaflet</div>
+  <div class="h-screen overflow-hidden">
+    <div class="h-full" id="map"></div>
+  </div>
 </template>
